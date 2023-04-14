2b0731810afdf4a18e1adad59dd30f8bdbe49554
George Alapatt <galapatt@stevens.edu>
2023-04-14 00:20:30 -0400

    Add creds to run.py

--- run.py
+++ run.py
@@ -156,8 +156,8 @@ def main(trader):
 
 
 if __name__ == '__main__':
-    with shift.Trader("test001") as trader:
-        trader.connect("initiator.cfg", "password")
+    with shift.Trader("algorizzms") as trader:
+        trader.connect("initiator.cfg", "FaF101o1a5")
         sleep(1)
         trader.sub_all_order_book()
         sleep(1)
